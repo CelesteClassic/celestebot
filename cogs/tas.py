96df3eefb517707fe1e0bfcdd7ac40de9d556556
cominixo01 <22600756+cominixo@users.noreply.github.com>
2021-03-06 22:18:42 -0300

    add uploadtas command for mods

--- cogs/tas.py
+++ cogs/tas.py
@@ -16,25 +16,23 @@ import zipfile
 import os
 import shutil
 from pathlib import Path
+import traceback
 
 
 # shoutouts to gonen
-def updateAndCommit(filePath, game, category):
-    fileName=os.path.split(filePath)[1]
+def updateAndCommit(data, game, category):
     home = str(Path.home())
     gitPath=home+'/tasdatabase'
     repo = git.Repo(gitPath)
     #repo.git.fetch('--all')
     #repo.git.reset('--hard', 'origin/master')
 
-    with open(filePath, 'r') as f:
-        data=f.read()
-        inputs=list(map(int,data[data.index("]")+1:].split(',')[:-1]))
-        framecount=len(inputs)-1
-        dashnum=0
-        for i in inputs:
-            if i&32:
-                dashnum+=1
+    inputs=list(map(int,data[data.index("]")+1:].split(',')[:-1]))
+    framecount=len(inputs)-1
+    dashnum=0
+    for i in inputs:
+        if i&32:
+            dashnum+=1
 
     jsonPath=os.path.join(gitPath,'database.json')
     with open(jsonPath,'r') as f:
@@ -90,6 +88,9 @@ def run():
 
 class Tas(commands.Cog):
 
+    async def is_mod(ctx):
+        return ctx.author.guild_permissions.manage_channels
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -143,158 +144,24 @@ class Tas(commands.Cog):
                         await ctx.send(embeds=[embed])
                     break
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        return
-        if message.channel.id == 548203844992237578:
-            if len(message.attachments) > 0:
-                file = message.attachments[0]
-		
-                if file.filename.endswith(".tas"):
-
-                    home = str(Path.home())
-
-                    gitPath=home+'/tasdatabase'
-                    repo = git.Repo(gitPath)
-                    repo.git.stash()
-                    repo.git.pull()
-
-                    game_detected = 'classic'
-                    category_detected = 'any'
-                    secondary_category = None
-
-                    if message.content:
-
-                        # haha python
-                        try:
-     
-                            game_detected = next(v for k, v in self.games.items() if any(game.lower() in message.content.lower() for game in k))
-
-                        except StopIteration:
-                            game_detected = 'classic'
-
-                        it = (v for k, v in self.categories.items() if any(category.lower() in message.content.lower() for category in k))
-
-                        for i in it:
-                            if i == "gemskip":
-                                category_detected = i
-                                break
-                            secondary_category = i
-                        
-
-                        if category_detected != "gemskip":
-                            category_detected = secondary_category
-                            secondary_category = None
-                        elif secondary_category == None:
-                            secondary_category = "any"
-                    
-                            
-                    base_category = None
-
-                    if category_detected != "gemskip":
-                        for k, v in self.base_categories.items():
-                            if category_detected in k:
-                                base_category = v
-                                break
-                        
-                    if secondary_category:
-                        category_detected += secondary_category
-                        base_category = secondary_category
-                   
-                    await message.channel.send(f'I see a TAS file! The detected category is {game_detected} {category_detected} with the base category {base_category}, please confirm this by sending a message with "yes" or "no", if you do not wish to upload this file, say "no" (or just do nothing)')
-                    def check(m):
-                        return ('no' in m.content.lower() or 'yes' in m.content.lower()) and m.channel == message.channel and m.author == message.author
-                    
-                    try:
-                        msg = await self.bot.wait_for('message', check=check, timeout=30)
-                    except TimeoutError:
-                        await message.channel.send("Okay, this has been cancelled, for possible games and categories type !categories")
-                        return
-                    
-                    if "no" in msg.content.lower():
-                        await message.channel.send("Okay, this has been cancelled, for possible games and categories type !categories")
-                        return
-
-                    os.system(f"cp {home}/tasdatabase/{game_detected}/{base_category}/*.tas {home}/UniversalClassicTas/CelesteTAS/TAS")
-                    try:
-                        os.system(f"cp {home}/tasdatabase/{game_detected}/{category_detected}/*.tas {home}/UniversalClassicTas/CelesteTAS/TAS")
-                    except:
-                        await message.channel.send("This category doesn't exist for this game (yet?)")
-                        return
-
-                    loop = asyncio.get_running_loop()
-                    result = await loop.run_in_executor(None, run)      
-                    oldtime = datetime.strptime(result.decode("utf-8").split("(")[0][:-1][:10], "%M:%S.%f")
-
-                    filePath = f"{home}/tasdatabase/{game_detected}/{category_detected}/" + file.filename
-                    await file.save(filePath)
-
-                    os.system(f"cp {home}/tasdatabase/{game_detected}/{base_category}/*.tas {home}/UniversalClassicTas/CelesteTAS/TAS")
-                    try:
-                        os.system(f"cp {home}/tasdatabase/{game_detected}/{category_detected}/*.tas {home}/UniversalClassicTas/CelesteTAS/TAS")
-                    except:
-                        await message.channel.send("This category doesn't exist for this game (yet?)")
-                        return
-
-                    with open(filePath, 'r') as f:
-                        data=f.read()
-                        inputs=list(map(int,data[data.index("]")+1:].split(',')[:-1]))
-                        framecount=len(inputs)-1
-                        dashnum=0
-                        for i in inputs:
-                            if i&32:
-                                dashnum+=1
-
-                    
-
-                    embed = discord.Embed(colour=0xffff00)
-                    embed.colour = 0xffff00
-                    embed.title = f"{int(file.filename[3:-4])*100}m - Playback in progress..."
-                    embed.add_field(name="Level frames", value=framecount)
-                    embed.add_field(name="Total dashes", value=dashnum)
-                    embed.add_field(name="Game", value=game_detected.capitalize())
-                    embed.add_field(name="Category", value=category_detected.capitalize())
-
-                    
-                    msg = await message.channel.send(embed=embed)
-                    loop = asyncio.get_running_loop()
-                    result = await loop.run_in_executor(None, run)
-
-                    if result is None:
-                        await msg.send("There was a death while executing this TAS, aborting.")
-                        return
-                
-                    newtime = datetime.strptime(result.decode("utf-8").split("(")[0][:-1][:10], "%M:%S.%f")
-
-                    delta = oldtime-newtime
-
-                    newtime_str = datetime.strftime(newtime, "%M:%S.%f")
-                    
-                
-                    embed.colour = 0x32CD32
-                    embed.title = f"{int(file.filename[3:-4])*100}m - Playback finished!"
-                    embed.add_field(name="Final time", value=f"{newtime_str}")
-                    embed.add_field(name="Time saved", value=f"{delta}")
-
-                    await msg.edit(embed=embed)
-
-                    force = False
-
-                    if "--force" in message.content and message.author.guild_permissions.manage_channels:
-                        force = True
-                        
-
-                    if delta <= timedelta(0) and not force:
-                        await msg.channel.send(f"This file doesn't save time in {game_detected} {category_detected} and will not be uploaded!")
-                        return
-                    updateAndCommit(filePath, game_detected, category_detected)
-                    await msg.channel.send(f"The files were uploaded to the github!")
-
     """ Old commands, should remove after discord.py gets support for slash commands"""
     @commands.command()
     async def categories(self, ctx):
         await ctx.send(f"Possible games in the TAS database: {list(self.games.values())}\n\nPossible categories in the TAS database: {list(self.categories.values())}")
 
+    @commands.check(is_mod)
+    @commands.command(aliases=["updatetas", "sendtas"])
+    async def uploadtas(self, ctx, game, category):
+        if len(ctx.message.attachments) > 0:
+            try:
+                data = await ctx.message.attachments[0].read().decode("utf-8")
+                updateAndCommit(data, game, category)
+                await ctx.send("TAS File uploaded (probably)!")
+            except:
+                traceback.print_exc()
+                await ctx.send("Something went wrong while uploading the TAS file! (tell cominixo)")
+        else:
+            await ctx.send("Please include an attachment with the message")
 
     @commands.command()
     async def tas(self, ctx, game, category, *, levelname):
