729f9851377e8c664a9fa10da2c3f3d3461b3fcf
cominixo01 <22600756+cominixo@users.noreply.github.com>
2021-03-08 13:09:01 -0300

    save tas file before saving zip

--- cogs/tas.py
+++ cogs/tas.py
@@ -59,6 +59,9 @@ async def updateAndCommit(tasfile, data, game, category):
 
     rootPath = os.path.join(gitPath, game, category)
 
+    await tasfile.save(os.path.join(rootPath,fileName))
+    repo.index.add([os.path.join(rootPath,fileName),jsonPath,zipPath])
+
 
     zipPath=os.path.join(rootPath,f"Full{game.capitalize()}{category.capitalize()}.zip")
     with zipfile.ZipFile(zipPath,"w") as zf:
@@ -67,8 +70,7 @@ async def updateAndCommit(tasfile, data, game, category):
                 zf.write(os.path.join(rootPath,file),os.path.join("TAS",file))
 
     
-    await tasfile.save(os.path.join(rootPath,fileName))
-    repo.index.add([os.path.join(rootPath,fileName),jsonPath,zipPath])
+   
     commit_msg=f'updated {game} {category} {change["name"]} to be {framecount}f ({framecount-oldframes:+}f) (automated)'
     repo.index.commit(commit_msg)
     repo.remotes.origin.push()
